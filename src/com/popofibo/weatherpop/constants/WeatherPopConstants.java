daef103fa7421766308c4a0867c98151d5e966df
sordidcan <popo.fibo@gmail.com>
2013-12-11 09:31:57 +0530

    modified license

--- src/com/popofibo/weatherpop/constants/WeatherPopConstants.java
+++ src/com/popofibo/weatherpop/constants/WeatherPopConstants.java
@@ -1,3 +1,18 @@
+/**
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2013 http://popofibo.com <popo.fibo@gmail.com> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this code, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+**/
 package com.popofibo.weatherpop.constants;
 
 /**
